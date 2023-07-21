862d4f16b377119c29d3dd992f1532ebc0b513b5
Kiranlawakar123 <kiran.lawakar@clairvoyantsoft.com>
2023-07-21 15:21:34 +0530

    Added div, h1 and p tag

--- day_5/firstreact-app/src/App.js
+++ day_5/firstreact-app/src/App.js
@@ -3,21 +3,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <h1>This is My React Js Application</h1>
+    <p>I am currently learning React Js</p>
     </div>
   );
 }
